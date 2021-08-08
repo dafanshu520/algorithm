591bcca5a5315843b4ec06e7052b35fdfd043f1b
dafanshu520 <544298923@qq.com>
2021-08-08 22:25:41 +0800

    Update 01week.go

--- 01week.go
+++ 01week.go
@@ -1,5 +1,5 @@
-20. 有效的括号
-https://leetcode-cn.com/problems/valid-parentheses/
+// 20. 有效的括号
+// https://leetcode-cn.com/problems/valid-parentheses/
 
 func isValid(s string) bool {
     n := len(s)
@@ -23,4 +23,21 @@ func isValid(s string) bool {
         }
     }
     return len(stack) == 0
+}
+
+
+// 70. 爬楼梯
+// https://leetcode-cn.com/problems/climbing-stairs/
+
+func climbStairs(n int) int {
+    if n <= 2 {
+        return n
+    }
+    f1, f2 := 1, 2
+    for i := 3; i <= n; i++ {
+        f3 := f2 + f1
+        f1 = f2
+        f2 = f3
+    }
+    return f2
 }
